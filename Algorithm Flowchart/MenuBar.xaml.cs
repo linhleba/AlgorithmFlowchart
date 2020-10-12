63445f2594e398dd095bc9659422bc016aa626f3
Dieu Le <18520265@gm.uit.edu.vn>
2020-10-12 22:31:58 +0700

    add user control

--- Algorithm Flowchart/MenuBar.xaml.cs
+++ Algorithm Flowchart/MenuBar.xaml.cs
@@ -1,7 +1,5 @@
-﻿using Algorithm_Flowchart;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,35 +8,31 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CopyAndPasteInCanvas
-
+namespace Algorithm_Flowchart
 {
-
-    public partial class Window1
-
+    /// <summary>
+    /// Interaction logic for MenuBar.xaml
+    /// </summary>
+    public partial class MenuBar : UserControl
     {
-        public Window1()
-
+        public MenuBar()
         {
             InitializeComponent();
-            DataContext = new ShapeDesigner().Canvas;
         }
-           
+
+        private void TabablzControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-           
-        }
-        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
-        {
-            Application.Current.Shutdown();
+
         }
     }
-
 }
