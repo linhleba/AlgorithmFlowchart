f75db0bd1fcbf878bb2fed3f1c48037adb44747b
Dieu Le <18520265@gm.uit.edu.vn>
2020-12-27 02:28:44 +0700

    update break arrow

--- Algorithm Flowchart/ArrowAdorner.cs
+++ Algorithm Flowchart/ArrowAdorner.cs
@@ -18,6 +18,7 @@ namespace Algorithm_Flowchart
     {
         public Point From { get; set; }
         public Point To { get; set; }
+        public List <Point> pList { get; set; }
 
         public ArrowAdorner(UIElement adornedElement)
             : base(adornedElement)
@@ -32,14 +33,24 @@ namespace Algorithm_Flowchart
             adornedElementRect.Y1 = From.Y;
             adornedElementRect.X2 = To.X;
             adornedElementRect.Y2 = To.Y;
+            Point top = new Point((From.X+To.X)/2, (From.Y + To.Y) / 2);
             // Some arbitrary drawing implements.
             SolidColorBrush renderBrush = new SolidColorBrush(Colors.Green);
             renderBrush.Opacity = 0.2;
             Pen renderPen = new Pen(new SolidColorBrush(Colors.Navy), 1.5);
             double renderRadius = 5.0;
             // Draw a circle at each corner.
+            if(pList!=null)
+            {
+                for (int i = 0; i < pList.Count; i++)
+                {
+                    drawingContext.DrawEllipse(renderBrush, renderPen, pList[i], renderRadius, renderRadius);
+                }
+            }
+            
             drawingContext.DrawEllipse(renderBrush, renderPen, new Point(adornedElementRect.X1-2.5, adornedElementRect.Y1-2.5), renderRadius, renderRadius);
             drawingContext.DrawEllipse(renderBrush, renderPen, new Point(adornedElementRect.X2, adornedElementRect.Y2), renderRadius, renderRadius);
+            drawingContext.DrawEllipse(renderBrush, renderPen, top, renderRadius, renderRadius);
         }
     }
 }
